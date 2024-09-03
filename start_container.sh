2616a60d661fb3620b396691f35da84eba53e3a3
Aashish cyber <ranaaashish000@gmail.com>
2024-09-03 18:39:58 +0530

    Create start_container.sh

---
+++ start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker push aashishrana8077/my-python-app
+
+# Run the Docker image as a container
+docker run -d -p 80:80 aashishrana8077/my-python-app
