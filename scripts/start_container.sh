1342e1b262b5d4da621ae7e54b92ee8937d370da
Aashish cyber <ranaaashish000@gmail.com>
2024-09-03 18:29:07 +0530

    Update start_container.sh
    Add This to the code

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,9 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+
+docker push aashishrana8077/my-python-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 80:80 aashishrana8077/my-python-app
+
