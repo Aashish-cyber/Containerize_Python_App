eb1124288c72ed58fdc2aa3cd1ba0f911ed689a3
Aashish cyber <ranaaashish000@gmail.com>
2024-09-04 16:32:53 +0530

    Update start_flask.sh

--- scripts/start_flask.sh
+++ scripts/start_flask.sh
@@ -10,7 +10,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push aashishrana8077/my-python-app
+docker push aashishrana8077/my-python-app:latest
 # Run the Docker image as a container
-docker run -d -p 80:80 aashishrana8077/my-python-app
+docker run -d -p 80:80 aashishrana8077/my-python-app:latest
 
