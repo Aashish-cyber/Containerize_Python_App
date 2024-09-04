ab769a128d7efef20cee583f4e1262b0adbb69cf
Aashish Cyber <ranaaashish@gmail.com>
2024-09-04 13:05:34 +0530

    Add deployment scripts

---
+++ scripts/Install_dependensies.sh
@@ -0,0 +1,4 @@
+!/bin/bash
+sudo yum install -y python3-pip
+pip3 install -r /home/ec2-user/my-python-app/requirements.txt
+
---
+++ scripts/start_flask.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/ec2-user/my-python-app
+nohup python3 app.py &
+
