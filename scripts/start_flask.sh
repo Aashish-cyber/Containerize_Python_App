921f7eb6d71d57e38d0d9ca16e956b418013fe79
Aashish cyber <ranaaashish000@gmail.com>
2024-09-04 14:08:06 +0530

    Update start_flask.sh

--- scripts/start_flask.sh
+++ scripts/start_flask.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#Activate the virtual environemt
+source /home/ubuntu/my-python-app/venv/bin/activate
+
+# Navigate to the application directory
 cd /home/ubuntu/my-python-app
+
+#start Aplication
 nohup python3 app.py &
 
