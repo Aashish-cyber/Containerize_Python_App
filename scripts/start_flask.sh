7d9154a3d2894efa0d3af155a3d240541415d845
Aashish cyber <ranaaashish000@gmail.com>
2024-09-04 15:24:46 +0530

    Update start_flask.sh

--- scripts/start_flask.sh
+++ scripts/start_flask.sh
@@ -1,10 +1,17 @@
-#!/bin/bash
+@#!/bin/bash
 #Activate the virtual environemt
-source /home/ubuntu/my-python-app/venv/bin/activate
+#source /home/ubuntu/my-python-app/venv/bin/activate
 
 # Navigate to the application directory
-cd /home/ubuntu/my-python-app
+#cd /home/ubuntu/my-python-app
 
 #start Aplication
-nohup python3 app.py &
+#nohup python3 app.py &
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker push aashishrana8077/my-python-app
+# Run the Docker image as a container
+docker run -d -p 80:80 aashishrana8077/my-python-app
 
