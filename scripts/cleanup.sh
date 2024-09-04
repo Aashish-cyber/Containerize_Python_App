5a32e869184c424a41092d31628dfe120067b0de
Aashish cyber <ranaaashish000@gmail.com>
2024-09-04 15:04:10 +0530

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-# Remove existing files to avoid conflicts
-if [ -f /home/ubuntu/my-python-app/scripts/Install_dependensies.sh ]; then
-    rm /home/ubuntu/my-python-app/scripts/Install_dependensies.sh
-fi
-
-if [ -f /home/ubuntu/my-python-app/scripts/start_flask.sh ]; then
-    rm /home/ubuntu/my-python-app/scripts/start_flask.sh
-fi
-
-# Remove specific files that might conflict
-#if [ -f /home/ubuntu/my-python-app/requirements.txt ]; then
-    #rm /home/ubuntu/my-python-app/requirements.txt
-#fi
+# Remove all files and directories in the target directory
+rm -rf /home/ubuntu/my-python-app/*
