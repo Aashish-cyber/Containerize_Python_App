3de100721f4e91662c11f220e6fe35eddfbf967d
Aashish cyber <ranaaashish000@gmail.com>
2024-09-04 14:24:57 +0530

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# Remove the specific file if it exists
+# Remove existing files to avoid conflicts
+if [ -f /home/ubuntu/my-python-app/scripts/Install_dependensies.sh ]; then
+    rm /home/ubuntu/my-python-app/scripts/Install_dependensies.sh
+fi
+
 if [ -f /home/ubuntu/my-python-app/scripts/start_flask.sh ]; then
     rm /home/ubuntu/my-python-app/scripts/start_flask.sh
 fi
