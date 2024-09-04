6204dafbb806d911b1d18246676f2aa7086b9cb6
Aashish cyber <ranaaashish000@gmail.com>
2024-09-04 14:20:29 +0530

    Update Install_dependensies.sh

--- scripts/Install_dependensies.sh
+++ scripts/Install_dependensies.sh
@@ -1,10 +1,18 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install -y python3-pip python3-venv
-#create a virtual enviornment
-python3 -m venv /home/ubuntu/my-python-app/venv
-#Activate Virtual enviornment
-source /home/ubuntu/my-python-app/venv/bin/activate
 
+#!/bin/bash
+
+# Update package lists
+sudo apt-get update
+
+# Install python3-pip and python3-venv if not already installed
+sudo apt-get install -y python3-pip python3-venv
+
+# Create a virtual environment if not already created
+if [ ! -d /home/ubuntu/my-python-app/venv ]; then
+    python3 -m venv /home/ubuntu/my-python-app/venv
+fi
+
+# Activate the virtual environment and install dependencies
+source /home/ubuntu/my-python-app/venv/bin/activate
 pip install -r /home/ubuntu/my-python-app/requirements.txt
 
